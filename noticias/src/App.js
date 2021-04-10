9b75ccccde02a67cfce4f8dd86c31a22bdfe2d3f
Ide Sistemas <fabimendo@hotmail.com>
2021-04-10 17:43:04 -0500

    Initial Upload

--- noticias/src/App.js
+++ noticias/src/App.js
@@ -1,8 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
+
+  // definir la categoría y noticias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  // cuando haya un cambio en la categoría, se vuelve a ejecutar el componente
+  useEffect(()=>{
+    const consultarAPI = async() =>{
+      const url = `https://newsapi.org/v2/top-headlines?country=ca&category=${categoria}&apiKey=e05f2d0a1d374c6bb2edf4325c0a7e19`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria]);
+
   return (
     <Fragment>
       <Header
@@ -10,7 +28,13 @@ function App() {
       ></Header>
 
       <div className="container white">
-        <Formulario />
+        <Formulario
+          guardarCategoria={guardarCategoria}  
+        />
+
+        <ListadoNoticias 
+          noticias={noticias}
+        />
       </div>
     </Fragment>
   );
--- noticias/src/components/Formulario.js
+++ noticias/src/components/Formulario.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Formulario.module.css';
+import useSelect from '../hooks/useSelect';
 
-const Formulario = () => {
+const Formulario = ({guardarCategoria}) => {
+
+    const OPCIONES = [
+        {value : 'genera', label: 'General'},
+        {value : 'business', label: 'Negocios'},
+        {value : 'entertainment', label: 'Entretenimiento'},
+        {value : 'health', label: 'Salud'},
+        {value : 'science', label: 'Ciencia'},
+        {value : 'sports', label: 'Deportes'},
+        {value : 'technology', label: 'Tecnología'}
+    ];
+
+    // utilizar curtom hook
+    const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
+
+    // submit al form, pasar categoría a app.js
+    const buscarNoticias = e => {
+        e.preventDefault();
+
+        guardarCategoria(categoria);
+    }
+
     return ( 
         <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
-                <form>
+                <form
+                    onSubmit={buscarNoticias}>
                     <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
+
+                    <SelectNoticias />
 
                     <div className="input-field col s12">
                         <input 
@@ -20,5 +46,9 @@ const Formulario = () => {
         </div>
      );
 }
- 
+
+Formulario.propTypes = {
+    guardarCategoria: PropTypes.func.isRequired
+}
+
 export default Formulario;

706897166c9c9115122d662d23ccbe6538237ff3
Ide Sistemas <fabimendo@hotmail.com>
2021-04-22 17:41:24 -0500

    Recetas Updated

--- bebidas/src/components/Receta.js
+++ bebidas/src/components/Receta.js
@@ -1,6 +1,61 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import {ModalContext} from '../context/ModalContext';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      position: 'absolute',
+      width: 450,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 const Receta = ({receta}) => {
+
+    // configuración del modal de material-ui
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+    const classes = useStyles();
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    // extraer los valores del context
+    const {informacion, guardarIdReceta, guardarReceta} = useContext(ModalContext);
+
+    // muestra y formatea los ingredientes
+    const mostrarIngredientes = informacion => {
+        let ingredientes = [];
+
+        for(let i = 1; i < 16; i++){
+            if(informacion[`strIngrediente${i}`]){
+                ingredientes.push(
+                    <li>{informacion[`strIngredient${i}`]}: {informacion[`strMeasure${i}`]}</li>
+                )
+            }
+        }
+
+        return ingredientes;
+    }
+    
     return ( 
         <div className="col-md-4 mb-3">
             <div className="card">
@@ -11,9 +66,37 @@ const Receta = ({receta}) => {
                     <button
                         type="button"
                         className="btn btn-block btn-primary"
+                        onClick={()=>{
+                            guardarIdReceta(receta.idDrink);
+                            handleOpen();
+                        }}
                     >
                         Ver Receta
                     </button>
+
+                    <Modal
+                        open={open}
+                        onClose={()=>{
+                            guardarIdReceta(null);
+                            guardarReceta({});
+                            handleClose();
+                        }}
+                    >
+                        <div style={modalStyle} className={classes.paper}>
+                            <h2>{informacion.strDrink}</h2>
+                            <h3 className="mt-4">Instrucciones</h3>
+                            <p>
+                                {informacion.strInstructions}
+                            </p>
+
+                            <img className="img-fluid my-4" src={informacion.strDrinkThumb} />
+
+                            <h3>Ingredientes y cantidades</h3>
+                            <ul>
+                                {(mostrarIngredientes(informacion))}
+                            </ul>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
--- bebidas/src/context/ModalContext.js
+++ bebidas/src/context/ModalContext.js
@@ -5,8 +5,32 @@ import axios from 'axios';
 export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
+
+    // state del provider
+    const [idreceta, guardarIdReceta] = useState(null);
+    const [informacion, guardarReceta] = useState({});
+
+    // una vez tenemos una receta, llamar la api
+    useEffect(()=>{
+        const obtenerReceta = async()=>{
+            if(!idreceta) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+
+            const resultado = await axios.get(url);
+            
+            guardarReceta(resultado.data.drinks[0]);
+        }
+        obtenerReceta();
+    },[idreceta])
+
     return ( 
-        <ModalContext.Provider>
+        <ModalContext.Provider
+            value={{
+                informacion,
+                guardarIdReceta,
+                guardarReceta
+            }}>
             {props.children}
         </ModalContext.Provider>
      );

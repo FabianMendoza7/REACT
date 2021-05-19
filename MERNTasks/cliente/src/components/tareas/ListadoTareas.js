2458ab02920829f820f7f83f8000654a9ddc9aa3
unknown <fmendoza.ospina7@outlook.com>
2021-05-19 15:47:48 -0500

    Updating Reducer

--- MERNTasks/cliente/src/components/tareas/ListadoTareas.js
+++ MERNTasks/cliente/src/components/tareas/ListadoTareas.js
@@ -6,10 +6,13 @@ const ListadoTareas = () => {
 
     // extraer proyectos de state inicial
     const proyectosContext = useContext(ProyectoContext);
-    const {proyecto} = proyectosContext;    
+    const {proyecto, eliminarProyecto} = proyectosContext;    
 
-    // Array destroctoring para extraer el proyecto actual
-    const {proyectoActual} = proyecto;
+    // Si no hay proyecto seleccionado
+    if(!proyecto) return <h2>Selecciona un proyecto</h2>;
+
+    // Array destructoring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;
 
     const tareasProyecto = [
         {nombre: 'Elegir Plataforma', estado: true},
@@ -18,9 +21,14 @@ const ListadoTareas = () => {
         {nombre: 'Elegir otro...', estado: true}
     ];
 
+    // Elimina un proyecto
+    const onClickEliminar = () => {
+        eliminarProyecto(proyectoActual.id)
+    }
+
     return ( 
         <Fragment>
-            <h2>Proyecto: {proyectoActual}</h2>
+            <h2>Proyecto: {proyectoActual.nombre}</h2>
 
             <ul>
                 {
@@ -34,7 +42,8 @@ const ListadoTareas = () => {
                 }
                 <button
                     type="button"
-                    className="btn btn-eliminar">
+                    className="btn btn-eliminar"
+                    onClick={onClickEliminar}>
                     Eliminar Proyecto &times;
                 </button>                
             </ul>

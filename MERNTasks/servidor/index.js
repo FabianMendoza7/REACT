222d4161152af2407e9327f62d4278a61d965e88
unknown <fmendoza.ospina7@outlook.com>
2021-06-06 17:17:48 -0500

    Creating index.js

--- MERNTasks/servidor/index.js
+++ MERNTasks/servidor/index.js
@@ -1,1 +1,17 @@
-console.log("holaaaaa");
+const express = require('express');
+
+// Crear el servidor.
+const app = express();
+
+// Puerto de la app.
+const PORT = process.env.PORT || 4000;
+
+// Definir la página ppal.
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+
+// Arrancar el server.
+app.listen(PORT, () => {
+    console.log(`El servidor está corriendo por el puerto ${PORT}`);
+});

1aee1dc24c943b7bfacccd434cd67d0fa3b07e96
fmendoza.ospina7 <fmendoza.ospina7@outlook.com>
2022-05-19 06:46:14 -0500

    Platzi courses

---
+++ platzi/react-hooks/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Characters from './components/Characters';
+
+function App()
+{
+  return (
+    <div className="App">
+      <Header />
+      <Characters />
+      <h1>Hola Mundo</h1>
+    </div>
+  );
+}
+
+export default App;

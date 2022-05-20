f07184118bcf6c4f55831e424d842fb436be5371
fmendoza.ospina7 <fmendoza.ospina7@outlook.com>
2022-05-20 10:02:20 -0500

    Platzi courses

--- platzi/react-hooks/src/components/Characters.jsx
+++ platzi/react-hooks/src/components/Characters.jsx
@@ -1,8 +1,29 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer, useMemo, useRef } from 'react'
+
+const initialState = {
+    favorites: []
+}
+
+const favoriteReducer = (state, action) =>
+{
+    switch (action.type)
+    {
+        case 'ADD_TO_FAVORITE':
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            }
+        default:
+            return state;
+    }
+}
 
 const Characters = () =>
 {
     const [characters, setCharacters] = useState([]);
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+    const [search, setSearch] = useState('');
+    const searchInput = useRef(null);
 
     useEffect(() =>
     {
@@ -11,11 +32,59 @@ const Characters = () =>
             .then(data => setCharacters(data.results));
     }, []);
 
+    const handleClick = favorite =>
+    {
+        dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
+    }
+
+    // Antes de usar useRef:
+    // const handleSearch = (event) =>
+    // {
+    //     setSearch(event.target.value);
+    // }
+
+    //Usando useRef:
+    const handleSearch = () =>
+    {
+        setSearch(searchInput.current.value);
+    }
+
+    // Antes de memoizar:
+    // const filteredUsers = characters.filter((user) =>
+    // {
+    //     return user.name.toLowerCase().includes(search.toLowerCase());
+    // })
+
+    //Memoizando:
+    const filteredUsers = useMemo(() =>
+        characters.filter((user) =>
+        {
+            return user.name.toLowerCase().includes(search.toLowerCase());
+        }),
+        [characters, search]
+    );
+
     return (
         <div className="Characters">
+
             {
-                characters.map(character => (
-                    <h2>{character.name}</h2>
+                favorites.favorites.map(favorite => (
+                    <li key={favorite.id}>
+                        {favorite.name}
+                    </li>
+                ))
+            }
+
+            <div className="Search">
+                <input type="text" value={search} ref={searchInput} onChange={handleSearch} />
+            </div>
+
+            {
+                filteredUsers.map(character => (
+                    <div className="item" key={character.id}>
+                        <h2>{character.name}</h2>
+                        <button type="button" onClick={() => handleClick(character)}>Agregar a Favoritos</button>
+                    </div>
                 ))
             }
         </div>
--- platzi/react-hooks/src/components/Header.jsx
+++ platzi/react-hooks/src/components/Header.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import ThemeContext from '../context/ThemeContext';
 
 const Header = () =>
 {
     const [darkMode, setDarkMode] = useState(false);
+    const color = useContext(ThemeContext);
+
     const handleClick = () =>
     {
         setDarkMode(!darkMode);
@@ -10,7 +13,7 @@ const Header = () =>
 
     return (
         <div className='Header'>
-            <h1>ReactHooks</h1>
+            <h1 style={{ color }}>ReactHooks</h1>
             <button type='button' onClick={handleClick}>{darkMode ? 'Dark Mode' : 'Light Mode'}</button>
         </div>
     )

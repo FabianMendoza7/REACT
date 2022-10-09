15038f78d17c9f15d75a18b2d594a7170d88f659
fmendoza.ospina7 <fmendoza.ospina7@outlook.com>
2022-10-09 09:40:43 -0500

    Citas React

--- V2/citas-react/src/App.jsx
+++ V2/citas-react/src/App.jsx
@@ -1,17 +1,21 @@
+import { useState } from 'react'
 import Formulario from './components/Formulario'
 import Header from "./components/Header"
 import ListadoPacientes from './components/ListadoPacientes'
 
 function App()
 {
-  //const [count, setCount] = useState(0)
+  const [pacientes, setPacientes] = useState([])
 
   return (
-    <>
+    <div className="container mx-auto mt-20">
       <Header />
-      <Formulario />
-      <ListadoPacientes />
-    </>
+
+      <div className="mt-12 md:flex">
+        <Formulario />
+        <ListadoPacientes />
+      </div>
+    </div>
   )
 }
 
---
+++ V2/citas-react/src/components/Paciente.jsx
@@ -0,0 +1,30 @@
+
+const Paciente = () =>
+{
+    return (
+        <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Nombre: {''}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Propietario: {''}
+                <span className="font-normal normal-case">Juan</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Email: {''}
+                <span className="font-normal normal-case">correo@correo.com</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Fecha Alta: {''}
+                <span className="font-normal normal-case">10 Dic 2022</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Síntoma: {''}
+                <span className="font-normal normal-case">No come jhjh jjhj kjwss</span>
+            </p>
+        </div>
+    )
+}
+
+export default Paciente

6a4b0306b190cbfeb0a9ad1f3d9c3f16ad19eea6
unknown <fmendoza.ospina7@outlook.com>
2022-10-25 05:51:01 -0500

    React

--- V2/criptos-react/src/components/Formulario.jsx
+++ V2/criptos-react/src/components/Formulario.jsx
@@ -14,6 +14,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
     &:hover {
         background-color: #7A7DFE;
         cursor: pointer;
@@ -21,12 +22,12 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
-  const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
+  const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
   
   return (
     <form>
         <SelectMonedas />
-        
+
         <InputSubmit 
             type="submit"
             value="Cotizar"
--- V2/criptos-react/src/hooks/useSelectMonedas.jsx
+++ V2/criptos-react/src/hooks/useSelectMonedas.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
 import styled from '@emotion/styled';
 
-const label = styled.label`
+const Label = styled.label`
     color: #FFF;
+    display: block;
+    font-family: 'Lato', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
+`
+const Select = styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 14px;
+    border-radius: 5px;
 `
 
 const useSelectMonedas = (label, opciones) => {
+    const [state, setState] = useState('')
+
     const SelectMonedas = () => (
         <>
-            <label>{label}</label>
-            <select>
+            <Label>{label}</Label>
+            <Select
+                value={state}
+                onChange={e => setState(e.target.value)}
+            >
                 <option value="">Seleccione</option>
                 {opciones.map(opcion => (
                     <option
@@ -17,11 +33,11 @@ const useSelectMonedas = (label, opciones) => {
                         value={opcion.id}
                     >{opcion.nombre}</option>  
                 ))}
-            </select>
+            </Select>
         </>
     )
     
-    return [ SelectMonedas ];
+    return [ state, SelectMonedas ];
 }
 
 export default useSelectMonedas

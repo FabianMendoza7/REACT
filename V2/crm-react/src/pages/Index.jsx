145e0367cbbe3e76199b1b0d4ca096a747e0ccf7
unknown <fmendoza.ospina7@outlook.com>
2022-11-05 11:06:42 -0500

    React

--- V2/crm-react/src/pages/Index.jsx
+++ V2/crm-react/src/pages/Index.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Index = () => {
   return (
-    <div>Clientes</div>
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>
+        Clientes
+      </h1>
+      <p className='mt-3'>Administra tus Clientes</p>
+    </>
   )
 }
 

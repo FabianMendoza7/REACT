e8ec4713848af7608a94bc0c03d3bfd1523a5bfd
fmendoza.ospina7 <fmendoza.ospina7@outlook.com>
2022-11-06 08:29:01 -0500

    React

--- V2/crm-react/src/pages/NuevoCliente.jsx
+++ V2/crm-react/src/pages/NuevoCliente.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const NuevoCliente = () => {
+const NuevoCliente = () =>
+{
+  const navigate = useNavigate();
+
   return (
-    <div>NuevoCliente</div>
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>
+        Nuevo Cliente
+      </h1>
+      <p className='mt-3'>Llena todos los camos para registrar un nuevo cliente</p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+          onClick={() => navigate(-1)}
+        >
+          Volver
+        </button>
+
+      </div>
+
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10'>
+
+      </div>
+    </>
   )
 }
 

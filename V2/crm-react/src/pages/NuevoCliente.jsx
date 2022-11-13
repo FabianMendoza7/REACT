8ec9fd1d76ee943a5270381b6a1449c1e452bb58
unknown <fmendoza.ospina7@outlook.com>
2022-11-12 19:15:05 -0500

    React

--- V2/crm-react/src/pages/NuevoCliente.jsx
+++ V2/crm-react/src/pages/NuevoCliente.jsx
@@ -1,14 +1,28 @@
-import { useNavigate, Form } from 'react-router-dom'
+import { useNavigate, Form, useActionData } from 'react-router-dom'
 import Formulario from '../components/Formulario';
+import Error from '../components/Error';
 
 export async function action({request}){
   const formData = await request.formData()
   const datos = Object.fromEntries(formData)
+
+  // Validación.
+  const errores = []
+  if(Object.values(datos).includes(''))
+  {
+    errores.push('Todos los campos son obligatorios')
+  }
+
+  // Retornar datos si hay errores.
+  if(Object.keys(errores).length){
+    return errores;
+  }
 }
 
 const NuevoCliente = () =>
 {
   const navigate = useNavigate();
+  const errores = useActionData();
 
   return (
     <>
@@ -28,6 +42,12 @@ const NuevoCliente = () =>
       </div>
 
       <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        
+        {errores?.length && errores.map((error, i) => 
+          <Error key={i}>
+            {error}
+          </Error>
+        )}
         <Form
           method='post'
         >

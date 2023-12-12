962437bcda542a279c7ca6b39217d1be7edd4679
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2023-12-12 07:50:58 -0500

    Creating stylesheet

---
+++ V2/guitarla-remix/app/root.tsx
@@ -0,0 +1,48 @@
+import {
+    Meta,
+    Links
+} from "@remix-run/react";
+import styles from './styles/index.css';
+
+export function meta(){
+    return [
+        {
+            charset: 'utf-8',
+            title: 'GuitarLA - Remix',
+            viewport: "width=device-width,initial-scale=1"
+        }
+    ]
+}
+
+export function links(){
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
+
+export default function App(){
+    return(
+        <Document>
+            <h1>
+                Holaaaa
+            </h1>
+        </Document>
+    )
+}
+
+function Document({children}: any){
+    return(
+        <html lang="es">
+            <head>
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}

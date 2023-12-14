6ac64e97048e39701a9f24ddd2ba6cf7078a5487
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2023-12-14 17:13:04 -0500

    More root configuration

--- V2/guitarla-remix/app/root.tsx
+++ V2/guitarla-remix/app/root.tsx
@@ -18,8 +18,25 @@ export function links(){
     return [
         {
             rel: 'stylesheet',
+            href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+        },
+        {
+            rel: 'stylesheet',
             href: styles
-        }
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com',
+            crossOrigin: "true"
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Comfortaa&family=Roboto:wght@400;700;900&family=Staatliches&display=swap'
+        },            
     ]
 }
 

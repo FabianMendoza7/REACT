41fc8334a27b54ddded32e8ae42e6d54d54b7551
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2023-12-22 13:46:32 -0500

    Adding route tienda.tsx

--- V2/guitarla-remix/app/routes/tienda.tsx
+++ V2/guitarla-remix/app/routes/tienda.tsx
@@ -1,3 +1,9 @@
+export async function loader(){
+    const respuesta = await fetch(`${process.env.API_URL}/guitars?populate=image`);
+    const resultado = await respuesta.json();
+    return {};
+}
+
 function Tienda(){
     return(
         <div>Tienda</div>

ff53e1257c5f0050b9bd59a484a82ae2e11f3b14
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2023-12-27 07:40:59 -0500

    Connecting with the API

--- V2/guitarla-remix/app/models/guitars.server.js
+++ V2/guitarla-remix/app/models/guitars.server.js
@@ -2,3 +2,8 @@ export async function getGuitars() {
     const respuesta = await fetch(`${process.env.API_URL}/guitars?populate=image`);
     return await respuesta.json();
 }
+
+export async function getGuitar(url) {
+    const respuesta = await fetch(`${process.env.API_URL}/guitars?filters[url]=${url}&populate=imagen`)
+    return await respuesta.json();
+}

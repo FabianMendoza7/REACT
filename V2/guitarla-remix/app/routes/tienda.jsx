24a32701d1ad9dcc1439cb8ffbb96109e993a63c
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2023-12-30 10:28:37 -0500

    Adding styles to Guitarras component

--- V2/guitarla-remix/app/routes/tienda.jsx
+++ V2/guitarla-remix/app/routes/tienda.jsx
@@ -1,6 +1,23 @@
 import { useLoaderData} from "@remix-run/react";
 import { getGuitarras } from "~/models/guitarras.server";
 import Guitarra from "~/components/guitarra";
+import styles from "~/styles/guitarras.css";
+
+export function meta() {
+    return {
+        title: 'GuitarLA - Tienda de Guitarras',
+        description: 'GuitarLA - Nuestra colección de guitarras'
+    }
+}
+
+export function links(){
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
 
 export async function loader(){
     const guitarras = await getGuitarras();
@@ -8,7 +25,7 @@ export async function loader(){
 }
 
 function Tienda(){
-     const guitarras = useLoaderData();
+    const guitarras = useLoaderData();
     return(
         <main className="contenedor">
             <h2 className="heading">Nuestra Colección</h2>

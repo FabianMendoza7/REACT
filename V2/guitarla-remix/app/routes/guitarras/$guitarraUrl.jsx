a66f2375beec93990907bc33b84e7af306e461a8
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2024-01-02 07:28:28 -0500

    Getting routes dinamically from Strapi API

---
+++ V2/guitarla-remix/app/routes/guitarras/$guitarraUrl.jsx
@@ -0,0 +1,16 @@
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarra } from "~/models/guitarras.server";
+
+export async function loader({request, params}){
+    const { guitarraUrl } = params;
+    const guitarra = await getGuitarra(guitarraUrl);
+
+    return guitarra;
+}
+
+export default function Guitarra() {
+    const guitarra = useLoaderData();
+return (
+    <div>$guitarraUrl</div>
+  )
+}

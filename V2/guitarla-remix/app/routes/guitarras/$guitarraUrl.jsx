d83e24be5c62ac9023ff1659eef3ca003a26fd42
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2024-01-06 15:18:46 -0500

    Adding meta information to the guitarra page

--- V2/guitarla-remix/app/routes/guitarras/$guitarraUrl.jsx
+++ V2/guitarla-remix/app/routes/guitarras/$guitarraUrl.jsx
@@ -1,5 +1,22 @@
 import { useLoaderData } from "@remix-run/react";
 import { getGuitarra } from "~/models/guitarras.server";
+import styles from "~/styles/guitarras.css";
+
+export function meta({data}) {
+    return {
+        title: `GuitarLA - ${data.data[0].attributes.name}`,
+        description: `Guitarras, venta de guitarras, guitarra ${data.data[0].attr.name}`
+    }
+}
+
+export function links() {
+    return [
+        {
+            rel: "stylesheet",
+            href: styles
+        }
+    ]
+}
 
 export async function loader({request, params}){
     const { guitarraUrl } = params;
@@ -10,7 +27,16 @@ export async function loader({request, params}){
 
 export default function Guitarra() {
     const guitarra = useLoaderData();
-return (
-    <div>$guitarraUrl</div>
+    const { name, description, image, precio } = guitarra.data[0].attributes;
+    return (
+        <main className='contenedor guitarra'>
+            <img className="imagen" src={image.data.attributes.url} alt={`Imagen de la guitarra ${name}`}></img>
+
+            <div className="contenido">
+                <h3>{name}</h3>
+                <p className="texto">{description}</p>
+                <p className="precio">$ {price}</p>
+            </div>
+        </main>
   )
 }

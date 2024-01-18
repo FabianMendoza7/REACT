05f624d93f92add3b9f63adbfb39aae61c299bea
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2024-01-18 10:13:58 -0500

    Finishing the blog

--- V2/guitarla-remix/app/routes/posts/$postUrl.jsx
+++ V2/guitarla-remix/app/routes/posts/$postUrl.jsx
@@ -1,4 +1,30 @@
+import { useLoaderData} from '@remix-run/react';
 import { getPost} from '~/models/posts.server';
+import { formatearFecha } from '~/utils/helpers';
+import styles from "~/styles/blog.css";
+
+export function links() {
+  return [
+      {
+          rel: "stylesheet",
+          href: styles
+      }
+  ]
+}
+
+export function meta({data}) {
+  if(!data){
+      return {
+          title: `GuitarLA - Entrada No Encontrada`,
+          description: `Guitarras, venta de guitarras, guitarra no encontrada`
+      }
+  }
+
+  return {
+      title: `GuitarLA - ${data.data[0].attributes.title}`,
+      description: `Guitarras, venta de guitarras, guitarra ${data.data[0].attributes.title}`
+  }
+}
 
 export async function loader({params}){
     const {postUrl} = params;
@@ -14,8 +40,19 @@ export async function loader({params}){
     return post;
 }
 
-export default function $postUrl() {
+export default function Post() {
+  const post = useLoaderData();
+  const { title, content, image, publishedAt} = post?.data[0].attributes;
+
   return (
-    <div>$postUrl</div>
+    <article className='contenedor post mt-3'>
+        <img className="imagen" src={image?.data?.attributes.url} alt={`Imagen del blog ${title}`}></img>
+
+        <div className="contenido">
+            <h3>{title}</h3>
+            <p className="fecha">{formatearFecha(publishedAt)}</p>
+            <p className="texto">$ {content}</p>
+        </div>      
+    </article>
   )
 }

4c040555f23b0aa6d9bdf450ca67e66de3e1b76d
GreenCodeRepos <fmendoza.ospina7@gmail.com>
2024-03-07 11:45:25 -0500

    Working with the shoppingcar

--- V2/guitarla-remix/app/routes/carrito.jsx
+++ V2/guitarla-remix/app/routes/carrito.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import styles from "~/styles/carrito.css"
+
+export function links(){
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export function meta(){
+  return {
+    title: 'GuitarLA - Carrito de Compras',
+    description: 'Venta de guitarras, música, blog, carrito de compras, tienda'
+  }
+}
 
 export default function Carrito() {
   return (
-    <div>Carrito</div>
+    <main className="contenedor">
+      <h1 className="heading">
+        Carrito de Compras
+
+        <div className="contenido">
+          <div className='carrito'>
+            <h2>Artículos</h2>
+          </div>
+        </div>
+
+        <aside className="resumen">
+          <h3>Resumen del Pedido</h3>
+          <p>Total a pagar: $</p>
+        </aside>
+      </h1>
+    </main>
   )
 }

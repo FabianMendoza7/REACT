e2b4a7b48f1f989b7ca87b0b4256b04c2da77489
GreenCodeRepos <fmendoza.ospina7@gmail.com>
2024-03-14 16:10:01 -0500

    Adding items to the shoppingcar

--- V2/guitarla-remix/app/root.jsx
+++ V2/guitarla-remix/app/root.jsx
@@ -53,7 +53,23 @@ export default function App() {
     const [carrito, setCarrito] = useState([])
 
     const agregarCarrito = guitarra => {
-        setCarrito([...carrito, guitarra])
+        if(carrito.some(guitarraState => guitarraState.id === guitarra.id)) {
+            // Identificar el elemento duplicado.
+            const carritoActualizado = carrito.map(guitarraState => {
+                if(guitarraState.id === guitarra.id) {
+                    // Reescribir la cantidad.
+                    guitarraState.cantidad = guitarra.cantidad
+                }
+
+                return guitarraState
+            })
+
+            // Añadir al carrito.
+            setCarrito(carritoActualizado)
+        } else {
+            // Registro nuevo, agregar al carrito.
+            setCarrito([...carrito, guitarra])
+        }
     }
 
     return(
